c2f9f3c5056c01949e30dbadbc71fb675ed17c21
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-19 11:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// user browser name
+var browserName = navigator.userAgent;
+// version of browser
+var version = navigator.appVersion;
+
+// create div element
+var divElement = document.createElement("div");
+// set id for divelement
+divElement.id = "browser-info";
+// textcontent inside div
+divElement.textContent = "You are using " + browserName + " version " + version;
+// add div inside document
+document.body.appendChild(divElement);
+
+
